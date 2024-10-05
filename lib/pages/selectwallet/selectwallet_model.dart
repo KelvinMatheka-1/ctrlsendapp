0dfac4a365cc3dc42558520183332b2992509d68
Kelvin M Matheka <beingmatheka@gmail.com>
2024-10-05 18:07:11 +0300

    dependencies

--- lib/pages/selectwallet/selectwallet_model.dart
+++ lib/pages/selectwallet/selectwallet_model.dart
@@ -8,7 +8,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:math';
 import 'selectwallet_widget.dart' show SelectwalletWidget;
-import 'package:carousel_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
